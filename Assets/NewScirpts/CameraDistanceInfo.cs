3d153e4da6b716ef86031d3cf4e6546963e3f07e
Yellow064 <alvaro_agb@hotmail.com>
2016-09-27 17:29:33 -0500

    Agregado calculo de distancia entre objectos y camara

---
+++ Assets/NewScirpts/CameraDistanceInfo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDistanceInfo : MonoBehaviour {
+    Transform ARCameraTransform;
+    Transform MyTransform;
+    float myDistance;
+	// Use this for initialization
+	void Start () {
+        ARCameraTransform = GameObject.Find("ARCamera").transform;
+        MyTransform = GameObject.Find("Betty").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float myDistance = Vector3.Distance(MyTransform.position, ARCameraTransform.position);
+        Debug.Log(myDistance);
+        Debug.Log("Say somehting hater");
+    }
+}
